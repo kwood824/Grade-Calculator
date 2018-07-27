be2d612b8e845cebd51f6367182d6864d3f4187e
kwood824 <kwood824@gmail.com>
2018-07-27 00:23:07 -0400

    Test commit

---
+++ Grade Calculator/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Test!" << std::endl;
+
+	std::cin.get();
+	//std::cin.get(); // I added these two to make the executable pause before closing when I compile at home
+
+	return 0;
+}
